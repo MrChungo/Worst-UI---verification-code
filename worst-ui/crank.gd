613b79e4c2caeaa8339e760ddcc8832703803db8
MrChungo <elchungo0518@gmail.com>
2026-05-20 12:55:51 -0400

    more physucs

--- worst-ui/crank.gd
+++ worst-ui/crank.gd
@@ -3,5 +3,13 @@ extends CharacterBody2D
 
 @export var speed: float
 
+@onready var target_position:Vector2 = get_global_mouse_position()
+
+var movement_tween = Tween.new()
+
+func _input(event: InputEvent) -> void:
+	if event
+
+@warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	pass
