48e6f45caab105dc35df902f35c89ed04336b973
MrChungo <elchungo0518@gmail.com>
2026-05-22 14:21:03 -0400

    eh

--- worst-ui/crank.gd
+++ worst-ui/crank.gd
@@ -1,6 +1,12 @@
 @tool
 extends CharacterBody2D
 
+var isGrabbed:bool = true
+
+var mouseEntered:bool = false
+
+var shortestDistanceToMouse:Vector2
+
 @export var speed: float
 
 @onready var target_position:Vector2 = get_global_mouse_position()
@@ -8,8 +14,33 @@ extends CharacterBody2D
 var movement_tween = Tween.new()
 
 func _input(event: InputEvent) -> void:
-	if event
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			#print(get_shortest_vector_to_mouse())
+			print(get_direction_vector(get_shortest_vector_to_mouse()))
+
+
 
 @warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
-	pass
+	if isGrabbed:
+		velocity = get_direction_vector(get_shortest_vector_to_mouse()) * speed
+		move_and_slide()
+	
+	
+
+func get_shortest_vector_to_mouse() -> Vector2:
+	return  get_global_mouse_position() - position
+
+func get_direction_vector(vector:Vector2) -> Vector2:
+	return global_position.direction_to(vector)
+
+
+
+
+func _on_mouse_entered() -> void:
+	mouseEntered = true
+
+
+func _on_mouse_exited() -> void:
+	mouseEntered = false
