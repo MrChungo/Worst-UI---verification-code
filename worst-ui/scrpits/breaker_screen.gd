9aed94568e901e0b87a9a8114ef0c3294b80ca74
MrChungo <elchungo0518@gmail.com>
2026-06-01 12:43:38 -0400

    sigh

--- worst-ui/scrpits/breaker_screen.gd
+++ worst-ui/scrpits/breaker_screen.gd
@@ -2,9 +2,24 @@ extends Node2D
 
 
 func _ready() -> void:
-	GlobalTime.get_node("5sTimer").timeout.connect(_on_5s_timer_timeout)
+	GlobalTime.get_node("1sTimer").timeout.connect(_on_5s_timer_timeout)
 
 
 func _on_5s_timer_timeout():
+	var switches:Array = $BreakerButtonsControl/GridContainer.get_children()
+	var looping  = true
+	var checks = 0
+	while looping:
 		
+		var switch = switches[randi() % switches.size()]
+		if switch.button_pressed:
+			switch.button_pressed = false
+			looping = false
+		checks += 1
+		if checks > 5:
+			looping = false
+	
+	
 	print("5s")
+
+	
