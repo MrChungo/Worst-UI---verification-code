5dc4a4618c692c55bf96cecbba954886f1a21f37
MrChungo <elchungo0518@gmail.com>
2026-06-01 23:12:32 -0400

    breaker screen works

--- worst-ui/scrpits/breaker_screen.gd
+++ worst-ui/scrpits/breaker_screen.gd
@@ -1,12 +1,33 @@
 extends Node2D
 
+signal power_online
+signal power_offline
 
 func _ready() -> void:
-	GlobalTime.get_node("1sTimer").timeout.connect(_on_5s_timer_timeout)
+	GlobalTime.get_node("2sTimer").timeout.connect(_on_5s_timer_timeout)
 	$BreakerButtonsControl.connect("button_pressed",on_button_pressed)
 
 
 func _on_5s_timer_timeout():
+	turn_off_random_switch()
+	
+	power_checks()
+	
+	
+	print("time ticks")
+
+func power_checks():
+	if $BreakerButtonsControl.has_deactivated_row():
+		power_offline.emit()
+		print("power_offline (Row Cleared)")
+	elif $BreakerButtonsControl.has_deactivated_column():
+		power_offline.emit()
+		print("power_offline (Column Cleared)")
+	else:
+		power_online.emit()
+		print("power_online")
+
+func turn_off_random_switch() -> void:
 	var switches:Array = $BreakerButtonsControl/GridContainer.get_children()
 	var looping  = true
 	var checks = 0
@@ -19,9 +40,7 @@ func _on_5s_timer_timeout():
 		checks += 1
 		if checks > 5:
 			looping = false
-	
-	
-	print("5s")
 
 func on_button_pressed():
-	print("hehe")
+	if randi_range(0,3) == 0:
+		turn_off_random_switch()
